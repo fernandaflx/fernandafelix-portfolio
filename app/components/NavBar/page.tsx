9e42299e40dd2d61132f4e6bdfdd5a0afa9ccc5f
Fernanda Felix <fernandaleitefelix@gmail.com>
2025-12-17 20:56:43 -0300

    fix navbar responsiveness

--- app/components/NavBar/page.tsx
+++ app/components/NavBar/page.tsx
@@ -1,4 +1,13 @@
+'use client'
+
+import { Twirl as Menu } from 'hamburger-react'
+import Image from 'next/image';
+import { useState } from "react";
+import ContactButton from './ContactButton/page';
+
 export default function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   const items = [
     { label: 'Sobre mim', href: '#about' },
     { label: 'Jornada', href: '#journey' },
@@ -6,34 +15,78 @@ export default function NavBar() {
   ]
 
   return (
-    <nav className="fixed top-6 left-0 right-0 z-50 flex justify-center px-4">
-      <div className="glass-panel rounded-full px-6 py-3 flex items-center gap-4 shadow-2xl shadow-accent-primary/5">
-        {/* TODO: Logo */}
-        <a className="flex items-center gap-2 group cursor-pointer" href="#">
-          <span className="font-bold text-gradient text-4xl">{'{'}</span>
-          <span className="font-bold text-white text-2xl">F</span>
-          <span className="font-bold  text-gradient text-4xl">{'}'}</span>
+    <nav className="fixed top-0 sm:top-6 left-0 right-0 z-50 flex justify-center sm:px-4">
+      <div className="hidden sm:flex">
+        <div className="glass-panel rounded-full px-6 py-3 flex items-center gap-4 shadow-2xl shadow-accent-primary/5">
+          <a href='#'>
+            <Image
+              src="/logo.png"
+              alt="Logo"
+              width={169}
+              height={145}
+              className="h-10 w-auto"
+            />
+          </a>
 
-        </a>
-        <div className="hidden md:flex items-center gap-1 p-1">
-          {items.map(({ label, href }) => (
-            <a
-              key={href}
-              className="px-4 py-1.5 rounded-full text-sm font-medium hover:text-white hover:bg-white/10 transition-all"
-              href={href}
-            >
-              {label}
-            </a>
-          ))}
+          <div className="hidden sm:flex items-center gap-1 p-1">
+            {items.map(({ label, href }) => (
+              <a
+                key={href}
+                className="px-4 py-1.5 rounded-full text-sm font-medium hover:text-white hover:bg-white/10 transition-all"
+                href={href}
+              >
+                {label}
+              </a>
+            ))}
+          </div>
+
+          <a
+            className="relative inline-flex h-9 overflow-hidden rounded-full p-px focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+            href="#contact"
+          >
+            <ContactButton />
+          </a>
         </div>
-        <a className="relative inline-flex h-9 overflow-hidden rounded-full p-px focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50" href="#contact">
-          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]"></span>
-          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-5 py-1 text-xs font-medium text-white backdrop-blur-3xl transition-colors hover:bg-slate-900">
-            Vamos conversar
-          </span>
-        </a>
       </div>
-    </nav>
 
+      <div
+        className={`flex sm:hidden w-full items-center justify-between p-3 ${isOpen ? "" : "glass-panel"
+          }`}
+      >
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          width={169}
+          height={145}
+          className="h-10 w-auto"
+        />
+        <Menu toggled={isOpen} toggle={setIsOpen} />
+      </div>
+
+      {isOpen && (
+        <div className="absolute w-screen -z-50 sm:hidden bg-bg-card">
+          <div className=" py-3 flex flex-col gap-1 shadow-2xl mt-16 shadow-accent-primary/5">
+            {items.map(({ label, href }) => (
+              <a
+                key={href}
+                href={href}
+                onClick={() => setIsOpen(false)}
+                className="py-4 px-4 text-sm font-medium hover:text-white hover:bg-white/10 transition-all"
+              >
+                {label}
+              </a>
+            ))}
+
+            <a
+              className="relative inline-flex h-9 w-3xs my-4 overflow-hidden rounded-full p-px focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 self-center sm:self-auto"
+              onClick={() => setIsOpen(false)}
+              href="#contact">
+              <ContactButton />
+            </a>
+          </div>
+        </div>
+
+      )}
+    </nav>
   )
 }
